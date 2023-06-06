c7da268e5c2f38799e15c1961685ba93df6fce6e
bennnil <bennnil-github@yahoo.com>
2023-06-06 10:19:06 +0200

    Abstand hinzugefügt

--- main.ts
+++ main.ts
@@ -1,33 +1,27 @@
+let posx = 0
+let posy = 0
+let Abstand = 0
+let tokx = 0
+let toky = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    let y = 0
+    let x = 0
+    posx = x
+    posy = y
+    Abstand = Math.abs(tokx - posx) + (toky - posy)
+    basic.showNumber(Abstand)
 })
 input.onGesture(Gesture.Shake, function () {
 	
 })
+input.onButtonPressed(Button.AB, function () {
+    toky = randint(0, 4)
+    tokx = randint(0, 4)
+    led.plot(tokx, toky)
+})
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    basic.pause(1000)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+	
 })
 basic.forever(function () {
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.toggle(x, y)
-            basic.pause(100)
-        }
-    }
+	
 })
